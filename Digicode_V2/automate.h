a177b9a525530df24bd85a72c285f406d4ecdce4
charlie38 <38466672+charlie38@users.noreply.github.com>
2018-04-20 08:08:24 +0000

    Create automate.h

---
+++ Digicode_V2/automate.h
@@ -0,0 +1,8 @@
+#ifndef __AUTOMATE__
+#define __AUTOMATE__
+
+void simule_automate() ;
+
+etat transition(etat Q, int e) ; /* retourne l'état atteint à partir de l'état Q avec l'entrée e */
+
+#endif
