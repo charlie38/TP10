82b5b278c3f16d70e3f3e4e3c102aa47d55fe301
charlie38 <38466672+charlie38@users.noreply.github.com>
2018-04-20 09:12:31 +0000

    Update automate.c

--- Digicode_V3/automate.c
+++ Digicode_V3/automate.c
@@ -40,21 +40,20 @@ char c ;
 	}
 }
 
-int transition[NB_ENTREES][NB_ETATS] {
-	for (int i=0, i<NB_ETATS,i++){
-		for (int j=0, j<NB_ENTREES, j++) {
-			if (j==1) {
-				transition[i][j]=1 ; /* remplit la colonne 1 */
-			}else{
-				transition[i][j]=0 ; /* initialise le reste à zéro */
-				}
-			}
-		}	
-	/* on complète le tableau */
-	transition[1][2]=2 ;
-	transition[2][3]=3 ;
-	transition[3][4]=4 ;
-}
+int transition[NB_ENTREES][NB_ETATS] ;
+for (int i=0, i<NB_ETATS,i++){
+	for (int j=0, j<NB_ENTREES, j++) {
+		if (j==1) {
+			transition[i][j]=1 ; /* remplit la colonne 1 */
+		}else{
+			transition[i][j]=0 ; /* initialise le reste à zéro */
+		}
+	}
+}	
+/* on complète le tableau */
+transition[1][2]=2 ;
+transition[2][3]=3 ;
+transition[3][4]=4 ;
 
 void simule_automate() {
 int etat_courant, etat_suivant, entree ;
@@ -62,7 +61,7 @@ int etat_courant, etat_suivant, entree ;
 	etat_courant = etat_initial() ;
 	while (entree != -1) {
 		entree = lire_entree() ;
-		etat_suivant=transition(etat_courant,entree) ;
+		etat_suivant=transition[etat_courant][entree] ;
 		/* test */
 		printf("%d - %d -> %d\n", etat_courant, entree, etat_suivant) ;
 		/* fin test */
